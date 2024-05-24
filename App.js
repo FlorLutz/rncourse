dc6c258de779f827bb0c0dd73f8aa64fd3af3382
Florian Lutz <florian.lutz@ymail.com>
2024-05-24 08:36:47 +0200

    first styles

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default function App() {
     <>
       <View style={styles.container}>
         <View>
-          <Text>Another piece!</Text>
+          <Text style={styles.dummyText}>Another piece!</Text>
         </View>
-        <Text>Hello World!</Text>
+        <Text style={styles.dummyText}>Hello World!</Text>
         <Button title="tap me!" />
       </View>
     </>
@@ -20,5 +20,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: "blue",
   },
 });
